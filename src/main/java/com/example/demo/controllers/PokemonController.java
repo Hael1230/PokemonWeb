aa8bfef82718409af95938ac549bbc456698b0c8
anthonny <hectoranthonnyelas@ufps.edu.co>
2023-12-20 09:38:54 -0500

    Servicio 2

---
+++ src/main/java/com/example/demo/controllers/PokemonController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.*;
+import com.example.demo.repository.*;
+
+@RestController 
+@RequestMapping("/pokemons")
+
+public class PokemonController {
+    
+    @Autowired
+	PokemonRepository pokemonRepository;
+
+    @GetMapping
+	public List<Pokemon>listar(){
+		return pokemonRepository.findAll();
+	}
+
+    @GetMapping("/{tipo}")
+	public Optional<Pokemon>findContieneById(@PathVariable Tipo_pokemon tipo_pokemon ){
+		Optional<Pokemon> pokemon=pokemonRepository.findByTipo_pokemon(tipo_pokemon);
+		if(pokemon.isPresent()) {
+			return pokemon;
+		}
+		return null;
+	}
+}
--- src/main/java/com/example/demo/repository/PokemonRepository.java
+++ src/main/java/com/example/demo/repository/PokemonRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 
 import com.example.demo.entities.Pokemon;
+import com.example.demo.entities.Tipo_pokemon;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
+
+    Optional<Pokemon> findByTipo_pokemon(Tipo_pokemon tipo_pokemon);
     
 }
